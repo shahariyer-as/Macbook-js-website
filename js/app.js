ede21c64f815f5730d112aa26d570a6de716df37
shahariyeras <shahariyer.as@gmail.com>
2021-08-21 23:45:55 +0600

    last commit

---
+++ js/app.js
@@ -0,0 +1,70 @@
+document.getElementById('8GB-memory').addEventListener('click', function () {
+    updateValue('memory-cost', 0)
+})
+
+document.getElementById('16gb').addEventListener('click', function () {
+    updateValue('memory-cost', 180)
+})
+
+document.getElementById('256GB-ssd').addEventListener('click', function () {
+    updateValue('storage-cost', 0)
+})
+
+document.getElementById('512GB-ssd').addEventListener('click', function () {
+    updateValue('storage-cost', 100)
+})
+
+document.getElementById('1tb-ssdstorage').addEventListener('click', function () {
+    updateValue('storage-cost', 180)
+})
+
+document.getElementById('delivery').addEventListener('click', function () {
+    updateValue('delivery-cost', 0)
+})
+
+document.getElementById('second-delivery').addEventListener('click', function () {
+    updateValue('delivery-cost', 20)
+})
+
+
+
+function updateValue(id, value) {
+    var costElement = document.getElementById(id)
+    costElement.innerText = value;
+
+    let bestPriceElement = document.getElementById('best-price')
+    let bestPrice = Number(bestPriceElement.innerText)
+
+    let extraMemoryElement = document.getElementById('memory-cost')
+    let extraMemoryCost = Number(extraMemoryElement.innerText)
+
+    let extraStorageElement = document.getElementById('storage-cost')
+    let extraStorageCost = Number(extraStorageElement.innerText)
+
+
+    let extraDeliveryElement = document.getElementById('delivery-cost')
+    let extraDeliveryCost = Number(extraDeliveryElement.innerText)
+
+    let extraTotalCost = bestPrice + extraMemoryCost + extraStorageCost + extraDeliveryCost;
+
+    let extraTotalElement = document.getElementById('total-price')
+    let inTotalElement = document.getElementById('in-total')
+    extraTotalElement.innerText = extraTotalCost;
+    inTotalElement.innerText = extraTotalCost;
+
+}
+
+document.getElementById('apply-btn').addEventListener('click', function () {
+    let totalElement = document.getElementById('total-price')
+    let inTotalElement = document.getElementById('in-total')
+    let promoInput = document.getElementById('promo-code')
+    let promoValue = promoInput.value
+    let totalAmount = Number(totalElement.innerText)
+
+    if (promoValue == 'stevekaku') {
+        let discount = parseInt((totalAmount / 100) * 20);
+        inTotalElement.innerText = totalAmount - discount;
+    }
+
+    promoInput.value = ''
+})
